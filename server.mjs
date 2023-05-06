0328a8a8acd46fcf72715cb255d10857df208199
OpenTech-Consult <opentech.consult@gmail.com>
2023-05-06 15:01:04 +0000

    Creating and HTML response
    We rarely have to deal with responses from web servers in plain text.
    We need to extend the previous code so that the server responds with a response in HTML just as a normal web server would.
    
    Adjusted the content type which is now text/html
    We dropped the response.write() method on line 6 resulting all ythe HTTP body to be sent entirely using the end method.

--- server.mjs
+++ server.mjs
@@ -1,10 +1,19 @@
-import { write } from 'fs';
 import { createServer } from 'http'
 
 const server = createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-    response.write('Hello ')
-    response.end(' World\n')
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    const body = `<!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Node.js Demo</title>
+    </head>
+    <body>
+    <h1 style="color:green">Hello World</h1>
+    </body>
+    </html>
+    `
+    response.end(body)
 })
 
 server.listen(8080, () => {
